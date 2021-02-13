f7f67d2069fed91ffe1d73fe00650c99409d660e
yaksplat <jimvrana@gmail.com>
2021-02-12 20:06:04 -0500

    Add files via upload

---
+++ StormPieces/AudioTester/AudioTester.ino
@@ -0,0 +1,186 @@
+// Specifically for use with the Adafruit Feather, the pins are pre-set here!
+
+// include SPI, MP3 and SD libraries
+
+#include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
+
+// These are the pins used
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+
+// Feather ESP8266
+#if defined(ESP8266)
+  #define VS1053_CS      16     // VS1053 chip select pin (output)
+  #define VS1053_DCS     15     // VS1053 Data/command select pin (output)
+  #define CARDCS          2     // Card chip select pin
+  #define VS1053_DREQ     0     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather ESP32
+#elif defined(ESP32)
+  #define VS1053_CS      32     // VS1053 chip select pin (output)
+  #define VS1053_DCS     33     // VS1053 Data/command select pin (output)
+  #define CARDCS         14     // Card chip select pin
+  #define VS1053_DREQ    15     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather Teensy3
+#elif defined(TEENSYDUINO)
+  #define VS1053_CS       3     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          8     // Card chip select pin
+  #define VS1053_DREQ     4     // VS1053 Data request, ideally an Interrupt pin
+
+// WICED feather
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define VS1053_CS       PC7     // VS1053 chip select pin (output)
+  #define VS1053_DCS      PB4     // VS1053 Data/command select pin (output)
+  #define CARDCS          PC5     // Card chip select pin
+  #define VS1053_DREQ     PA15    // VS1053 Data request, ideally an Interrupt pin
+
+#elif defined(ARDUINO_NRF52832_FEATHER )
+  #define VS1053_CS       30     // VS1053 chip select pin (output)
+  #define VS1053_DCS      11     // VS1053 Data/command select pin (output)
+  #define CARDCS          27     // Card chip select pin
+  #define VS1053_DREQ     31     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather M4, M0, 328, nRF52840 or 32u4
+#else
+  #define VS1053_CS       6     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          5     // Card chip select pin
+  // DREQ should be an Int pin *if possible* (not possible on 32u4)
+  #define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+
+#endif
+
+#define LED_PIN     12
+#define NUM_LEDS  61
+#define BRIGHTNESS 5
+const int buttonPin = 13; 
+int buttonState = 0; 
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
+
+void setup() {
+  Serial.begin(115200);
+
+  // if you're using Bluefruit or LoRa/RFM Feather, disable the radio module
+  //pinMode(8, INPUT_PULLUP);
+
+  // Wait for serial port to be opened, remove this line for 'standalone' operation
+  while (!Serial) { delay(1); }
+  delay(500);
+  Serial.println("\n\nAdafruit VS1053 Feather Test");
+  
+  if (! musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+
+  Serial.println(F("VS1053 found"));
+ 
+  musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+  
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+   while (1);  // don't do anything more
+  }
+  Serial.println("SD OK!");
+  
+  // list files
+  printDirectory(SD.open("/"), 0);
+  
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(10,10);
+  
+#if defined(__AVR_ATmega32U4__) 
+  // Timer interrupts are not suggested, better to use DREQ interrupt!
+  // but we don't have them on the 32u4 feather...
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+#else
+  // If DREQ is on an interrupt pin we can do background
+  // audio playing
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+#endif
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
+   pinMode(buttonPin, INPUT);
+  // Play a file in the background, REQUIRES interrupts!
+ // Serial.println(F("Playing full track 001"));
+ // musicPlayer.playFullFile("/track001.mp3");
+
+  //Serial.println(F("Playing track 002"));
+  //musicPlayer.startPlayingFile("/Cannibal.mp3");
+for(int i = 0; i < NUM_LEDS; i++)
+  {
+    strip.setPixelColor(i, 0, 0,0,255); 
+  }
+      strip.show();
+    // musicPlayer.startPlayingFile("/track003.mp3");
+}
+
+bool musicPlaying = false;
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  //Serial.println(F(buttonState));
+  if (buttonState == LOW  && musicPlaying == false) {
+    Serial.println(F("i should play music"));
+    musicPlayer.startPlayingFile("/track003.mp3");
+    musicPlaying = true;
+for(int i = 0; i < NUM_LEDS; i++)
+  {
+    strip.setPixelColor(i, 255, 0,0,0); 
+  }
+      strip.show();
+  }
+// if the button is pushed
+//start playing the song
+//set the lights to red
+
+
+  if (musicPlayer.stopped()) {
+    Serial.println(F("music has stopped"));
+    musicPlaying=false;
+  //get a new random time to wait for the next song
+for(int i = 0; i < NUM_LEDS; i++)
+  {
+    strip.setPixelColor(i, 0, 0,0,255); 
+  }
+      strip.show();
+  }
+  delay(100);
+}
+
+
+
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
